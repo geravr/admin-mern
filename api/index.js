adc8d8b1b16cf73d6e819af64da3c539dbed4612
Gerardo Villa <gera.villa21@gmail.com>
2021-02-01 02:05:13 -0600

    install and config cors

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 require("dotenv").config();
 const { dbConnection } = require("./db/config");
+const cors = require("cors");
 
 // Create express server
 const app = express();
 
-// DataBase
+// Database
 dbConnection();
+
+// CORS
+app.use(cors());
 
 // Parser body
 app.use(express.json());
